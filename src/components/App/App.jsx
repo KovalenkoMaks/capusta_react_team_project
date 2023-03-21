dab8a89035a7d2080582aed3e2429952417841d2
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-03-21 15:34:04 +0200

    He did Background

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,5 @@
+import HomePageView from 'pages/HomePageView/HomePageView'
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <HomePageView />
     </div>
   );
 };
