17f6abb22ce046a6554e78e637e92a048ee42b95
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-03-21 16:10:35 +0200

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,13 @@
+import Header from 'components/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import HomePageView from './HomePageView/HomePageView';
 
 const Layout = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+      <HomePageView />
       <Outlet />
     </Suspense>
   );
