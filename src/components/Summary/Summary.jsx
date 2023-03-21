e16aa97fec73c532f9f4e8b50e5994c0c81429c0
Mykola Pankov <pankoff97@gmail.com>
2023-03-21 15:15:23 +0200

    components/Summary+
    created getMonth.js, Summary, Summary.styled, index (jsx)

---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,55 @@
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
+import { transactionSelectors } from 'redux/transactions';
+import s from './Summary.styled.css';
+
+function Summary() {
+  const { pathname } = useLocation();
+  const transtype = pathname.slice(1);
+
+  const summaryIncomes = useSelector(transactionSelectors.getIncomeMonthStats);
+  const summaryExpenses = useSelector(
+    transactionSelectors.getExpenseMonthStats
+  );
+
+  const elements =
+    transtype === 'expense'
+      ? Object.entries(summaryExpenses)
+      : Object.entries(summaryIncomes);
+
+  const markup = elements
+    .map(([month, sum]) => {
+      if (sum === 'N/A') sum = 0;
+      return (
+        <li key={month} className={s.item}>
+          <span className={s.month}>{month}</span>
+          <span className={s.sum}>
+            {transtype === 'expense' ? '-' : '+'}
+            {fnForNumberDivide(sum)}.00
+          </span>
+        </li>
+      );
+    })
+    .reverse();
+
+  return (
+    <div className={s.wrap}>
+      <h2 className={s.title}>
+        {transtype === 'expense' ? 'Expenses' : 'Income'}
+      </h2>
+      <ul className={s.list}>{markup}</ul>
+    </div>
+  );
+}
+
+const fnForNumberDivide = number => {
+  if (number === undefined) {
+    return 0;
+  } else {
+    const res = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    return res;
+  }
+};
+
+export default Summary;
