eaaff93fb496c046b9327ed5c63d1a13e932e12d
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-22 14:03:04 +0200

    work with modal

---
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -0,0 +1,49 @@
+import { createPortal } from 'react-dom';
+import {
+  ModalContainer,
+  ModalContent,
+  ModalTitle,
+  ButtonContainer,
+  CloseButton,
+  Overlay,
+  OverlayContainer,
+} from './LogOutConfirmModal.styled';
+import {ReactComponent as CloseIcon} from 'images/closeIcon.svg'
+import Button from 'components/Button/Button';
+
+const modalElement = document.querySelector("#modal");
+export default function ConfirmModal({
+  onClick,
+  text,
+  handleConfirm,
+  handleCancel,
+}){
+return createPortal(
+  <Overlay>
+        <OverlayContainer>
+          <ModalContainer>
+            <ModalContent>
+              <ModalTitle>{text}</ModalTitle>
+              <ButtonContainer>
+                <Button
+                  text="Yes"
+                  type="button"
+                  width= "125px"
+                  onClick={handleConfirm}
+                />
+                <Button
+                  text="No"
+                  type="button"
+                  width= "125px"
+                  onClick={handleCancel}
+                />
+              </ButtonContainer>
+            </ModalContent>
+            <CloseButton type="button" onClick={onClick}>
+              <CloseIcon/>
+            </CloseButton>
+          </ModalContainer>
+        </OverlayContainer>
+      </Overlay>
+      ,modalElement);
+    }
