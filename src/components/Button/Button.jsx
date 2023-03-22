ed9e02cc824218c28509d4c5ea7f644d1316aab7
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-22 08:50:35 +0200

    create Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import { StyledBtn } from './Button.styled';
+
+export default function Button(){
+
+  return (
+    <>
+      <StyledBtn></StyledBtn>
+    </>
+  );
+}
---
+++ src/components/Button/Button.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const StyledBtn = styled.button`
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.02em;
+  width: 125px;
+  height: 44px;
+  padding: 12px 15px;
+  text-align: center;
+  font-weight: 700;
+  text-transform: uppercase;
+  border-radius: 16px;
+  border: 0px;
+
+
+  background-color: #ffffff;
+  cursor: pointer;
+
+    &:hover {
+    background-color: #ff751d;
+    color: #ffffff;
+  }
+ `;
