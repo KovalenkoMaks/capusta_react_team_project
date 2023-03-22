4e2ad076c1deb58cbcd14bc212e97b3f89b747e3
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-03-22 23:01:57 +0200

    first variant of table

--- src/components/ExpensesPage/Form/Form.jsx
+++ src/components/ExpensesPage/Form/Form.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as Calculator } from '../../../images/calculator.svg';
 import { DatePicker, Input, Dropdown, Button } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { FormContainer } from '../ExpensesPage.styled';
+import { TablePage } from '../TablePage/TablePage';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(customParseFormat);
@@ -51,22 +52,28 @@ export const Form = () => {
             </button>
           </Dropdown>
         </div>
-        <Input suffix={calcilatorIcon} placeholder="0,00" className="calc"  style={{marginRight: '32px'}}/>
+        <Input
+          suffix={calcilatorIcon}
+          placeholder="0,00"
+          className="calc"
+          style={{ marginRight: '32px' }}
+        />
 
-      <Button
-        type="primary"
-        style={{
-          marginRight: '16px',
-          width: '136px',
-          height: '44px',
-          backgroundColor: '#FF751D',
-        }}
-        className="submitBtn"
-      >
-        Input
-      </Button>
-      <Button style={{ width: '136px', height: '44px' }}>Clear</Button>
+        <Button
+          type="primary"
+          style={{
+            marginRight: '16px',
+            width: '136px',
+            height: '44px',
+            backgroundColor: '#FF751D',
+          }}
+          className="submitBtn"
+        >
+          Input
+        </Button>
+        <Button style={{ width: '136px', height: '44px' }}>Clear</Button>
       </div>
+      <TablePage />
     </FormContainer>
   );
 };
