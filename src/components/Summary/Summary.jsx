d00627867eac50eb977d3512b8acf69326167213
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-23 16:24:19 +0200

    Summary styles and work with moked datas

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,10 +1,42 @@
-import {SummaryContainer} from './Summary.styled.jsx'
+import {SummaryContainer, Header, SummaryTable, TableTbody, MonthTd, ValueTd, TableTr} from './Summary.styled.jsx';
+// import { useTransactions } from 'hooks/useTransactions.js';
 
 export default function Summary(){
-  
+  const monthStats = {
+    January: 50000,
+    February: 100,
+    March: "N/A",
+    April: "N/A",
+    May: 1,
+    June: "N/A",
+    July: 3,
+    August: "N/A",
+    September: "N/A",
+    October: 77,
+    November: "N/A",
+    December: 123
+  }
+  // const { monthsStats } = useTransactions();
+  // console.log(monthsStats);
   return(
     <SummaryContainer>
-
+     <Header>Summary</Header>
+     <SummaryTable>
+     <TableTbody>
+          { Object.entries(monthStats).map((element) => {
+             if(element[1] === 'N/A') {
+              return false;
+            } else {
+              return(
+                <TableTr key={`${element[0]}${element[1]}`}>
+                  <MonthTd>{element[0]}</MonthTd>
+                  <ValueTd>{element[1]}.00</ValueTd>
+                </TableTr>
+              );
+            }
+          })}
+        </TableTbody>
+     </SummaryTable>
     </SummaryContainer>
   )
 }
--- src/components/Summary/Summary.styled.jsx
+++ src/components/Summary/Summary.styled.jsx
@@ -1,15 +1,57 @@
 import styled from '@emotion/styled';
 
 export const SummaryContainer = styled.div`
- position: absolute;
- z-index: 2;
- right: 0;
- bottom: 0;
- transform: translate(-30px, 280px);
   display: block;
-  width: 230px;
-  height: 280px;
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  right: 0;
+  transform: translate(-32px, 268px);
+  display: block;
+  width: 213px;
+  height: 266px;
   overflow: hidden;
   border-radius: 20px 20px 20px 0;
-  background-color: #F5F6FB
+  text-transform: uppercase;
+  background-color: #f5f6fb;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 38px;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.02em;
+  border-bottom: 2px solid #ffffff;
+`;
+export const SummaryTable = styled.table`
+  background-color: #f5f6fb;
+  border-collapse: collapse;
+  width: 100%;
+`;
+export const TableTbody = styled.tbody`
+  border-left: none;
+`;
+export const MonthTd = styled.tbody`
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.04em;
+`;
+export const ValueTd = styled.tbody`
+  font-size: 12px;
+  line-height: 1.16;
+  letter-spacing: 0.04em;
+  /* padding-left: 20px; */
+`;
+export const TableTr = styled.tr`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 12px;
+  border-bottom: 2px solid #ffffff;
+  height: 38px;
+  text-align: left;
 `;
