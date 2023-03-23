ffcde630798632e811db2b1bc18c38f39fe48a49
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-23 16:31:06 +0200

    create translate hook

---
+++ src/hooks/useMonthTranslate.js
@@ -0,0 +1,30 @@
+export const rusToEng = name => {
+  switch (name) {
+    case 'Январь':
+      return 'January';
+    case 'Февраль':
+      return 'February';
+    case 'Март':
+      return 'March';
+    case 'Апрель':
+      return 'April';
+    case 'Май':
+      return 'May';
+    case 'Июнь':
+      return 'June';
+    case 'Июль':
+      return 'July';
+    case 'Август':
+      return 'August';
+    case 'Сентябрь':
+      return 'September';
+    case 'Октябрь':
+      return 'October';
+    case 'Ноябрь':
+      return 'November';
+    case 'Декабрь':
+      return 'December';
+    default:
+      break;
+  }
+};
