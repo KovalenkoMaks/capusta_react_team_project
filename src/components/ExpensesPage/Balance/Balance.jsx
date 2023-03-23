22251069dc7898293a50b639b48ef755480d9c30
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-03-23 02:30:09 +0200

    update button

--- src/components/ExpensesPage/Balance/Balance.jsx
+++ src/components/ExpensesPage/Balance/Balance.jsx
@@ -1,27 +1,30 @@
-import { Button, Typography, Input } from 'antd';
+import { Button, Typography, Input, Form } from 'antd';
 import { useAuth } from 'hooks/useAuth';
-import { useTransactions } from 'hooks/useTransactions';
 import { useDispatch } from 'react-redux';
 import { newBalance } from 'redux/transactions/operations';
 import { ReactComponent as Reports } from '../../../images/bar_chart.svg';
 import { BalanceContainer } from '../ExpensesPage.styled';
+import { useState } from 'react';
 
 export const Balance = () => {
-  const { transactions } = useTransactions();
   const { balance } = useAuth();
-  let disabled = transactions.length === 0 && balance === 0;
-  console.log(disabled);
-  // if (transactions.length === 0 || balance === 0) {
-  //   disabled = true;
-
-  //   console.log(disabled);
-  // }
+  const [value, setValue] = useState('');
 
   const dispatch = useDispatch();
-  const onSubmit = e => {
-    e.preventDefault();
-    console.log(e.target[0].value);
-    dispatch(newBalance(e.target[0].value));
+
+  const validateNumber = (rule, value) => {
+    return new Promise((resolve, reject) => {
+      if (!Number.isInteger(Number(value))) {
+        reject('Please enter an number');
+      } else {
+        resolve();
+      }
+    });
+  };
+
+  const handleSubmit = () => {
+    dispatch(newBalance(value));
+    setValue('');
   };
   return (
     <BalanceContainer>
@@ -29,23 +32,33 @@ export const Balance = () => {
         <Typography level={5} className="title">
           Balance:
         </Typography>
-        <form onSubmit={onSubmit}>
-          <Input
-            placeholder={`${balance} UAH`}
-            type="text"
-            disabled={!disabled}
-          />
-          {disabled ? (
-            <Button
+        <Form
+          name="number-form"
+          initialValues={{ number: value }}
+          onFinish={handleSubmit}
+        >
+          <Form.Item
+            name="number"
+            rules={[
+              {
+                validator: validateNumber,
+              },
+            ]}
+            noStyle
+          >
+            <Input
+              placeholder={`${balance} UAH`}
               type="text"
-              className="button"
-              disabled={disabled}
-              // htmlType={onSubmit}
-            >
-              Confirm
-            </Button>
-          ) : null}
-        </form>
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              disabled={balance !==0}
+            />
+          </Form.Item>
+
+          <Button type="text" disabled={value === '' || balance !==0} className="button" htmlType="submit">
+            Confirm
+          </Button>
+        </Form>
       </div>
 
       <Button type="text" className="reports">
