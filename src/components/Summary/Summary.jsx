c8020070f421aad18d9141637a8c228cb92dc623
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-03-24 21:52:51 +0200

    add summary

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,34 +1,33 @@
-import {SummaryContainer, Header, SummaryTable, TableTbody, MonthTd, ValueTd, TableTr} from './Summary.styled.jsx';
+import {
+  SummaryContainer,
+  Header,
+  SummaryTable,
+  TableTbody,
+  MonthTd,
+  ValueTd,
+  TableTr,
+} from './Summary.styled.jsx';
 // import { rusToEng } from 'hooks/useMonthTranslate';
-// import { useTransactions } from 'hooks/useTransactions.js';
+import { useTransactions } from 'hooks/useTransactions.js';
+import { useLocation } from 'react-router-dom';
 
-export default function Summary(){
-  const monthStats = {
-    January: 50000,
-    February: 100,
-    March: "N/A",
-    April: "N/A",
-    May: 1,
-    June: "N/A",
-    July: 3,
-    August: "N/A",
-    September: "N/A",
-    October: 77,
-    November: "N/A",
-    December: 123
-  }
-  // const { monthsStats } = useTransactions();
-  // console.log(monthsStats);
-  return(
+export default function Summary() {
+  const { monthsStats } = useTransactions();
+  const location = useLocation();
+  let monthStats = {};
+  location.pathname === '/home/expenses'
+    ? (monthStats = { ...monthsStats.expenses })
+    : (monthStats = { ...monthsStats.incomes });
+  return (
     <SummaryContainer>
-     <Header>Summary</Header>
-     <SummaryTable>
-     <TableTbody>
-          { Object.entries(monthStats).map((element) => {
-             if(element[1] === 'N/A') {
+      <Header>Summary</Header>
+      <SummaryTable>
+        <TableTbody>
+          {Object.entries(monthStats).map(element => {
+            if (element[1] === 'N/A') {
               return false;
             } else {
-              return(
+              return (
                 <TableTr key={`${element[0]}${element[1]}`}>
                   <MonthTd>{element[0]}</MonthTd>
                   <ValueTd>{element[1]}.00</ValueTd>
@@ -37,7 +36,7 @@ export default function Summary(){
             }
           })}
         </TableTbody>
-     </SummaryTable>
+      </SummaryTable>
     </SummaryContainer>
-  )
+  );
 }
