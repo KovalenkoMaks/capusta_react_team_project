14686fef062bf49649f78e1a57cc4b74c294116d
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-03-26 01:51:25 +0200

    add modal in progres

--- src/components/ExpensesPage/TablePage/TablePage.jsx
+++ src/components/ExpensesPage/TablePage/TablePage.jsx
@@ -7,8 +7,15 @@ import {
 import { ReactComponent as DeleteBtn } from '../../../images/deleteTable.svg';
 import { TableContainer, TableWrapper } from '../ExpensesPage.styled';
 import Summary from 'components/Summary/Summary';
+import ConfirmModal from 'components/ConfirmModal/ConfirmModal';
+import { useState } from 'react';
 
 export const TablePage = () => {
+  const [modal, setModal] = useState(false);
+  const closeModal = () => {
+    setModal(false);
+  };
+
   const dispatch = useDispatch();
   const deleteTransaction = id => {
     dispatch(delTransaction(id))
@@ -18,65 +25,79 @@ export const TablePage = () => {
       });
   };
   const { transactions } = useTransactions();
+  console.log(transactions.expenses);
+  const reversedArr = [...transactions.expenses].reverse();
+  // data.reverse();
+  console.log(reversedArr);
   return (
-    <TableWrapper>
-    <TableContainer>
-      <table>
-        <thead>
-          <tr className="tHead">
-            <th className="thDate">Date</th>
-            <th>Description</th>
-            <th>category</th>
-            <th>Sum</th>
-            <th className="dell"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.expenses.length === 0
-            ? [...Array(21)].map((_, index) => (
-                <tr key={index}>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              ))
-            : [
-                ...transactions.expenses.map(transaction => (
-                  <tr key={transaction._id}>
-                    <td className="date">{transaction.date}</td>
-                    <td className="decs">{transaction.description}</td>
-                    <td className="categ">{transaction.category}</td>
-                    <td className="amount">{transaction.amount}.00 UAH</td>
-                    <td className="del">
-                      <button
-                        type="button"
-                        onClick={() => {
-                          deleteTransaction(transaction._id);
-                        }}
-                      >
-                        {<DeleteBtn />}
-                      </button>
-                    </td>
-                  </tr>
-                )),
-                ...[
-                  ...Array(Math.max(0, 21 - transactions.expenses.length)),
-                ].map((_, index) => (
-                  <tr key={`empty-${index}`}>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                )),
-              ]}
-        </tbody>
-      </table>
-    </TableContainer>
-    <Summary/>
-    </TableWrapper>
+    <>
+      <TableWrapper>
+        <TableContainer>
+          <table>
+            <thead>
+              <tr className="tHead">
+                <th className="thDate">Date</th>
+                <th>Description</th>
+                <th>category</th>
+                <th>Sum</th>
+                <th className="dell"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {reversedArr.length === 0
+                ? [...Array(21)].map((_, index) => (
+                    <tr key={index}>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  ))
+                : [
+                    ...reversedArr.map(transaction => (
+                      <tr key={transaction._id}>
+                        <td className="date">{transaction.date}</td>
+                        <td className="decs">{transaction.description}</td>
+                        <td className="categ">{transaction.category}</td>
+                        <td className="amount" style={{ color: 'red' }}>
+                          -{transaction.amount}.00 UAH
+                        </td>
+                        <td className="del">
+                          <button
+                            type="button"
+                            onClick={() => {
+                              deleteTransaction(transaction._id);
+                            }}
+                          >
+                            {<DeleteBtn />}
+                          </button>
+                        </td>
+                      </tr>
+                    )),
+                    ...[...Array(Math.max(0, 21 - reversedArr.length))].map(
+                      (_, index) => (
+                        <tr key={`empty-${index}`}>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                        </tr>
+                      )
+                    ),
+                  ]}
+            </tbody>
+          </table>
+        </TableContainer>
+        <Summary />
+      </TableWrapper>
+      <ConfirmModal
+        onClick={closeModal}
+        text="Are you sure?"
+        handleConfirm={deleteTransaction(value)}
+        handleCancel={() => setModal(prevState => !prevState)}
+      />
+    </>
   );
 };
