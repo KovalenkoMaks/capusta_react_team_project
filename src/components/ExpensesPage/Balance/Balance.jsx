6ec8986288281263411177fdcc1253205e191d11
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-03-27 00:41:29 +0300

    helper

--- src/components/ExpensesPage/Balance/Balance.jsx
+++ src/components/ExpensesPage/Balance/Balance.jsx
@@ -7,20 +7,30 @@ import { newBalance } from 'redux/transactions/operations';
 import { ReactComponent as Reports } from '../../../images/bar_chart.svg';
 import { BalanceContainer } from '../ExpensesPage.styled';
 import { Link } from 'react-router-dom';
+import BalanceModal from './BalanceModal';
+import { useState } from 'react';
 
 export const Balance = () => {
   const { transactions } = useTransactions();
-  const { balance } = useAuth();
-
+  const { balance, isRefreshing } = useAuth();
+  const [promptClose, setPromptClose] = useState(true);
   let disabled = transactions.incomes.length === 0 && balance === 0;
-
   const dispatch = useDispatch();
+
   const onSubmit = (value, { resetForm }) => {
     // console.log(value);
     dispatch(newBalance(value));
     resetForm();
+    if (disabled) {
+      setPromptClose(prev => !prev);
+    }
   };
+
   const showReport = !window.location.href.endsWith('reports');
+
+  const toggleWindow = () => {
+    setPromptClose(prev => !prev);
+  };
 
   return (
     <BalanceContainer>
@@ -52,6 +62,8 @@ export const Balance = () => {
                   />
                 )}
               </Field>
+                  {!isRefreshing && promptClose && balance === 0 && (<BalanceModal onClose={toggleWindow}/>)}
+
               {disabled ? (
                 <Button type="text" htmlType="submit" className="button">
                   Confirm
---
+++ src/components/ExpensesPage/Balance/BalanceModal.jsx
@@ -0,0 +1,21 @@
+import { Title, Modal, Text, ClickOnMe } from './BalanceModal.styled';
+
+const BalanceModal = ({ onClose }) => {
+  const handleClickWindow = e => {
+    if (e.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <Modal onClick={handleClickWindow}>
+      <Title>
+        Hello! To get started, enter the current balance of your account!
+      </Title>
+      <Text>You can't spend money until you have it =) </Text>
+      <ClickOnMe>Click on me to close</ClickOnMe>
+    </Modal>
+  );
+};
+
+export default BalanceModal;
