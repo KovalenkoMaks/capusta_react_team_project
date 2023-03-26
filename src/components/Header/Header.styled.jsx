48eac791822e0882d4df8398836be14e97dd77bc
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-03-26 22:29:41 +0300

    fix header

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -9,7 +9,7 @@ export const Container = styled.header`
   width: 100%;
 
   @media (min-width: 768px){
-    max-width: 1280px;
+    max-width: 768px;
     padding: 10px 32px;
   }
   @media (min-width: 1280px) {
